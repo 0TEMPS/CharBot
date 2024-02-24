5fee2d88874eed74fdaf8dcca1ea758569ac3ad6
0TEMPS <144968215+0TEMPS@users.noreply.github.com>
2024-02-24 11:28:44 -0500

    Update CharBotDisplay.lua

--- CharBotDisplay.lua
+++ CharBotDisplay.lua
@@ -31,7 +31,7 @@ function CBD.CreateUi(Title)
 end
 
 function CBD.PingTest(ResponseTable, bottomtext)
-	local Status = Window:CreateTab("Status", 16093311060)
+	local Status = Window:CreateTab("Status")
 	local Section = Status:CreateSection("API Status")
 	
 	for i,v in pairs(ResponseTable) do
@@ -44,7 +44,7 @@ function CBD.PingTest(ResponseTable, bottomtext)
 end
 
 function CBD.ClientInfo(ClientInfo)
-	local Info = Window:CreateTab("Info", 16093328141)
+	local Info = Window:CreateTab("Info")
 	local Section = Info:CreateSection("Client Info")
 	-- Create an array to store the order of keys
 	local order = {"BotName", "BotUserID", "BotHumanoid", "ClientVersion", "CurrentEnvironment", "Executor", "ClientTimezone", "ClientStartTime", "RigType"}
@@ -55,7 +55,7 @@ function CBD.ClientInfo(ClientInfo)
 				local Label = Info:CreateLabel(tostring(i2).." = "..tostring(v2))
 			end
 		end
-	end
+	end 
 	local Section = Info:CreateSection("Server Info")
 	local order2 = {"ChatType", "PlaceID", "PlaceName", "UniverseID"}
 
@@ -68,7 +68,7 @@ function CBD.ClientInfo(ClientInfo)
 	end
 end
 function CBD.CreateCommandOutput()
-	Output = Window:CreateTab("Output", 16093328141)
+	Output = Window:CreateTab("Output")
 	local Section = Output:CreateSection("Output / Command Logs")
 	
 end
@@ -77,6 +77,57 @@ function CBD.Output(Text)
 	local Label = Output:CreateLabel(Text)
 	wait(10)
 	Label:Remove()
+end
+
+function CBD.CreateBotConfigTab()
+	BotConfig = Window:CreateTab("Bot Config")
+end
+
+function CBD.BotConfigSettings(BotConfigTable)
+	local Section = BotConfig:CreateSection("General Settings")
+	
+	local Label = BotConfig:CreateLabel("Owner = "..BotConfigTable["General Settings"]["Owner"])
+	
+	local Paragraph = BotConfig:CreateParagraph({Title = "Approval Words", Content = table.concat(BotConfigTable["General Settings"]["Approval Words"], ", ")})
+	
+	local Paragraph = BotConfig:CreateParagraph({Title = "Disapproval Words", Content = table.concat(BotConfigTable["General Settings"]["DisapprovalWords"], ", ")})
+	
+	local Paragraph = BotConfig:CreateParagraph({Title = "Greetings", Content = table.concat(BotConfigTable["General Settings"]["Greetings"], ", ")})
+	
+	local Label = BotConfig:CreateLabel("AutoJumpWhenSitting = "..tostring(BotConfigTable["General Settings"]["AutoJumpWhenSitting"]))
+	local Label = BotConfig:CreateLabel("Error-Logging = "..tostring(BotConfigTable["General Settings"]["Error-Logging"]))
+	local Label = BotConfig:CreateLabel("Log-Commands = "..tostring(BotConfigTable["General Settings"]["Log-Commands"]))
+	
+	local Label = BotConfig:CreateLabel("NativeCurrency = "..tostring(BotConfigTable["General Settings"]["NativeCurrency"]))
+	local Label = BotConfig:CreateLabel("CurrencySymbol = "..tostring(BotConfigTable["General Settings"]["CurrencySymbol"]))
+	
+	local Label = BotConfig:CreateLabel("PlayerLockBrickVector = "..tostring(BotConfigTable["General Settings"]["PlayerLockBrickVector"]))
+	
+	local Section = BotConfig:CreateSection("Chat Settings")
+	
+	local Label = BotConfig:CreateLabel("ChatPublicly = "..tostring(BotConfigTable["Chat Settings"].ChatPublicly))
+	local Label = BotConfig:CreateLabel("ChatLoadingOutputs = "..tostring(BotConfigTable["Chat Settings"].ChatLoadingOutputs))
+	local Label = BotConfig:CreateLabel("ChatStartupGreeting = "..tostring(BotConfigTable["Chat Settings"].ChatStartupGreeting))
+	local Label = BotConfig:CreateLabel("ChatErrorLogs = "..tostring(BotConfigTable["Chat Settings"].ChatErrorLogs))
+	local Label = BotConfig:CreateLabel("ChatPrefix = "..tostring(BotConfigTable["Chat Settings"].ChatPrefix))
+
+	local Section = BotConfig:CreateSection("API Keys")
+	
+	local Label = BotConfig:CreateLabel("APININJA_KEY = "..tostring(BotConfigTable["API Keys"].APININJA_KEY))
+	local Label = BotConfig:CreateLabel("RBLX_TRADE_CSRF = "..tostring(BotConfigTable["API Keys"].RBLX_TRADE_CSRF))
+	
+	
+end
+
+function CBD.CommandListTab()
+	CommandList = Window:CreateTab("Command List")
+end
+
+function CBD.AddCommands(CommandTable)
+	local What = CommandList:CreateSection("Command List")
+	for i,v in pairs(CommandTable) do
+		local Label = CommandList:CreateLabel(tostring(i))
+	end
 end
 
 return CBD
